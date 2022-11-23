7411d6241381571a9e07a12211eb4e9702bae462
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-11-24 02:40:22 +0600

    backend project initialize with expressjs

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config()
+
+import express from "express"
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+
+const whitelist = [process.env.FRONTEND]
+const corsOptions = {
+    credentials: true,
+    origin: function (origin, callback) {
+
+        if(whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+
+        } else {
+            // no access
+            callback(null, false)
+        }
+    }
+}
+
+app.use(cors(corsOptions))
+
+app.get("/", (req, res)=>{
+    res.send("hello world")
+})
+
+
+app.use((err, req, res)=>{
+    res.status(500).json({message: err.message})
+})
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT)
