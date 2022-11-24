4a17fd6c0d6c163ff8eeba6de5b24d92a55cbf0b
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2022-11-24 20:28:22 +0600

    added category route and get all category public route

---
+++ routes/categoryRoute.js
@@ -0,0 +1,20 @@
+import express from "express"
+import response from "../response";
+import Category from "../models/Category";
+
+
+const router = express.Router()
+
+// [GET]  api/v1/category find all categories
+router.get("/", async function (req, res, next) {
+    try {
+        let categories = await (await Category.collection).find({}).toArray()
+        response(res, categories, 200)
+    } catch (ex) {
+        next(ex)
+    }
+})
+
+
+
+export default router
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
-import express from "express";;
+import express from "express";
 import authRoute from "./authRoute";
 import watchRoute from "./watchRoute";
+import categoryRoute from "./categoryRoute";
 
 const router = express.Router()
 
 
 router.use("/api/v1/auth", authRoute)
 router.use("/api/v1/watch", watchRoute)
+router.use("/api/v1/category", categoryRoute)
 
 
 export default router
